292d3d35f7770dd56fc669f483149acf2d0318a3
Tim Clem <tim.clem@gmail.com>
2015-06-23 12:44:25 -0400

    Initial Commit

---
+++ ios-location-trivia-delegate/Resources/Other-Sources/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ios-location-trivia-delegate
+//
+//  Created by iOS Staff on 6/23/15
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FISAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FISAppDelegate class]));
+    }
+}
